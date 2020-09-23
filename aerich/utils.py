37843a9aa159308b3d10ae242a0171b05e705d83
ShirleyChau <71644406+ShirleyChau@users.noreply.github.com>
2020-09-23 15:45:49 +0800

    utils add ask_rename_column

--- aerich/utils.py
+++ aerich/utils.py
@@ -49,3 +49,13 @@ def get_tortoise_config(ctx: Context, tortoise_orm: str) -> dict:
             ctx=ctx,
         )
     return config
+
+def ask_rename_column(old_name:str,new_name:str)->bool:
+    while True:
+        flag = input(f"Do you rename {old_name} to {new_name}?(y/n)")
+        if flag.lower() == "y":
+            return True
+        elif flag.lower()=="n":
+            return False
+        else:
+            print("Please input y or n.")
