02fe5a9d3171af13d310bde73569d7bc32b4d1f8
AulonSal <aulonsal@gmail.com>
2021-02-20 13:11:29 +0530

    Close Tortoise connections properly

--- aerich/cli.py
+++ aerich/cli.py
@@ -36,6 +36,7 @@ def coro(f):
     def wrapper(*args, **kwargs):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(f(*args, **kwargs))
+        loop.run_until_complete(Tortoise.close_connections())
 
     return wrapper
 
