93bb90468d0927399e229ebb8ce26d56d2634414
Waket Zheng <waketzheng@gmail.com>
2025-02-26 10:10:40 +0800

    tests: poetry add

--- tests/test_python_m.py
+++ tests/test_python_m.py
@@ -1,6 +1,18 @@
+import subprocess  # nosec
+from pathlib import Path
+
 from aerich.version import __version__
-from tests._utils import run_shell
+from tests._utils import chdir, run_shell
 
 
 def test_python_m_aerich():
     assert __version__ in run_shell("python -m aerich --version")
+
+
+def test_poetry_add(tmp_path: Path):
+    package = Path(__file__).parent.resolve().parent
+    with chdir(tmp_path):
+        subprocess.run(["poetry", "new", "foo"])  # nosec
+        with chdir("foo"):
+            r = subprocess.run(["poetry", "add", package])  # nosec
+            assert r.returncode == 0
