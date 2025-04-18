72d18087b5b628d4c95818ff77d8e0d1552acfb6
panhq <42133281+hail-pas@users.noreply.github.com>
2025-04-18 11:25:34 +0800

    aerich init-db should create folder after validating app

--- aerich/__init__.py
+++ aerich/__init__.py
@@ -256,13 +256,6 @@ class Command(AbstractAsyncContextManager):
     async def init_db(self, safe: bool) -> None:
         location = self.location
         app = self.app
-        dirname = Path(location, app)
-        if not dirname.exists():
-            dirname.mkdir(parents=True)
-        else:
-            # If directory is empty, go ahead, otherwise raise FileExistsError
-            for unexpected_file in dirname.glob("*"):
-                raise FileExistsError(str(unexpected_file))
 
         await Tortoise.init(config=self.tortoise_config)
         connection = get_app_connection(self.tortoise_config, app)
