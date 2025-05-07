1ee06bdec82128afdfbca9a697994e20c5e46635
hail-pas <hypofiasco@gmail.com>
2025-05-07 11:15:22 +0800

    fix: catch empty old model exception and raise with a tip.

--- aerich/migrate.py
+++ aerich/migrate.py
@@ -172,7 +172,7 @@ class Migrate:
         if empty:
             return await cls._generate_diff_py(name)
         new_version_content = get_models_describe(cls.app)
-        last_version = cast(dict, cls._last_version_content) or {}
+        last_version = cast(dict, cls._last_version_content)
         cls.diff_models(last_version, new_version_content)
         cls.diff_models(new_version_content, last_version, False)
 
@@ -398,7 +398,13 @@ class Migrate:
         :return:
         """
         _aerich = f"{cls.app}.{cls._aerich}"
-        old_models.pop(_aerich, None)
+        try:
+            old_models.pop(_aerich, None)
+        except AttributeError:
+            # Invalid use when app migration directory exists but aerich table not exist
+            raise click.UsageError(
+                "You may need to run `aerich init-db` first to initialize the database."
+            )
         new_models.pop(_aerich, None)
         models_with_rename_field: set[str] = set()  # models that trigger the click.prompt
 
