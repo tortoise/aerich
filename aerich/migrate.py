da35d977c231351ad757e065b124ad1c78268355
Waket Zheng <waketzheng@gmail.com>
2025-08-30 10:40:57 +0800

    refactor: only postgres

--- aerich/migrate.py
+++ aerich/migrate.py
@@ -944,9 +944,10 @@ class Migrate:
         for _upgrade_fk_m2m_operator in cls._upgrade_fk_m2m_index_operators:
             if "ADD" in _upgrade_fk_m2m_operator or "CREATE" in _upgrade_fk_m2m_operator:
                 cls.upgrade_operators.append(_upgrade_fk_m2m_operator)
-                if m := re.search(r'CREATE TABLE ["`](\w+?)["`]', _upgrade_fk_m2m_operator):
+                if m := re.search(r'CREATE TABLE "(\w+?)"', _upgrade_fk_m2m_operator):
                     table_name = m.group(1)
-                    pattern = re.compile(rf'COMMENT ON TABLE ["`]{table_name}["`]')
+                    pattern = re.compile(rf'COMMENT ON TABLE "{table_name}"')
+                    # Comment of postgresql m2m table may set before creation of it
                     for index, sql in enumerate(cls.upgrade_operators[:-1]):
                         if pattern.search(sql):
                             sqls = sql.split(";")
