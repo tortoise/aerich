668de4abfaeb85b99f9e4f11828a4abe9c752484
Waket Zheng <waketzheng@gmail.com>
2025-10-09 10:40:54 +0800

    tests: prefer to use anyio instead of pytest-asyncio

--- tests/assets/sqlite_migrate/_tests.py
+++ tests/assets/sqlite_migrate/_tests.py
@@ -5,7 +5,7 @@ from models import Foo
 from tortoise.exceptions import IntegrityError
 
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_allow_duplicate() -> None:
     await Foo.all().delete()
     await Foo.create(name="foo")
@@ -14,14 +14,14 @@ async def test_allow_duplicate() -> None:
     await obj.delete()
 
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_unique_is_true() -> None:
     with pytest.raises(IntegrityError):
         await Foo.create(name="foo")
         await Foo.create(name="foo")
 
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_add_unique_field() -> None:
     if not await Foo.filter(age=0).exists():
         await Foo.create(name="0_" + uuid.uuid4().hex, age=0)
@@ -29,14 +29,14 @@ async def test_add_unique_field() -> None:
         await Foo.create(name=uuid.uuid4().hex, age=0)
 
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_drop_unique_field() -> None:
     name = "1_" + uuid.uuid4().hex
     await Foo.create(name=name, age=0)
     assert await Foo.filter(name=name).exists()
 
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_with_age_field() -> None:
     name = "2_" + uuid.uuid4().hex
     await Foo.create(name=name, age=0)
@@ -44,7 +44,7 @@ async def test_with_age_field() -> None:
     assert obj.age == 0
 
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_without_age_field() -> None:
     name = "3_" + uuid.uuid4().hex
     await Foo.create(name=name, age=0)
@@ -52,7 +52,7 @@ async def test_without_age_field() -> None:
     assert getattr(obj, "age", None) is None
 
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_m2m_with_custom_through() -> None:
     from models import FooGroup, Group
 
@@ -65,7 +65,7 @@ async def test_m2m_with_custom_through() -> None:
     assert not foo_group.is_active
 
 
-@pytest.mark.asyncio
+@pytest.mark.anyio
 async def test_add_m2m_field_after_init_db() -> None:
     from models import Group
 
